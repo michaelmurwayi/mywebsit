98c26eef788219619927b3f9ac617b4a756368f4
Michael Murwayi <mikemurwayi7@gmail.com>
2023-07-04 05:07:22 -0400

    ft-resune:Initial page template with navigation component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import Home from './Home/Home'
+import Resume from './Resume/Resume'
 
 class App extends React.Component{
   render(){
@@ -11,6 +12,7 @@ class App extends React.Component{
         <BrowserRouter>
           <Routes>
             <Route exact path='/' Component={Home}/>
+            <Route exact path='resume' Component={Resume}/>
           </Routes>
         </BrowserRouter>
       </div>
---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from '../Navigation/Nav'
+
+const Resume = () =>{
+    return (
+        <div>
+            <Nav/>
+        </div>
+    )
+}
+
+export default Resume
