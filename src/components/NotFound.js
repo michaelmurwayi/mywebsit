63f90404473825795f57549caa0410d0aa266c98
Michael Murwayi <mikemurwayi7@gmail.com>
2023-07-11 09:55:08 -0400

    ft-notfound:Final template for 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,14 +3,18 @@ import './NotFound.css'
 
 const NotFound = () =>{
     return (
-        <div className='not-found'>
-            <div className='header col-md-12'>
-                <h1> Error 404 ! </h1>
-                <h1>PAGE NOT FOUND</h1>
+        <div className='not-found row'>
+            <div className='image col-md-6'>
+
             </div>
-            <div className='back col-md-12'>
-                <p className=''>Back Home</p>
-                <hr/>
+            <div className='content-404 col-md-6'>
+
+                <div className='text'>
+                    <h1>Page Not Found</h1>
+                    <h2>Error 404</h2>
+                    <p>Go Back Home</p>
+                    <hr/>
+                </div>
             </div>
         </div>
     )
