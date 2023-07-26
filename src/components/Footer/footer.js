ee8080e1878b20ed29882e115a9fbabd88385e11
Michael Murwayi <mikemurwayi7@gmail.com>
2023-07-26 00:43:17 -0400

    fx-footer: Adding company name to footer copy right div

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -3,10 +3,11 @@ import React from 'react'
 
 const Footer = () =>{
     const currentYear = new Date().getFullYear();
+    const company = "huncho Inc";
     return (
         <footer class="footer">
         <div class="container">
-            <p>&copy; {currentYear} Your Company. All rights reserved.</p>
+            <p>&copy; {currentYear} {company}. All rights reserved.</p>
         </div>
         </footer>
 
