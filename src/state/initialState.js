e3ac87e0824c55c6f4dc700846d62e298f74e8ae
Michael Murwayi <mikemurwayi7@gmail.com>
2023-08-02 17:25:14 -0400

    ft-state: adding skills al state object

--- src/state/initialState.js
+++ src/state/initialState.js
@@ -2,7 +2,54 @@
 const initialState = {
     dev: "Michael Murwayi",
     role: "Full Stack Web Developer",
-    bio : "As a code composer, I dance with algorithms, orchestrating symphonies of efficiency and functionality. With each keystroke, I strive to create clean, scalable, and maintainable software architectures, harnessing the power of cutting-edge technologies and frameworks."
+    bio : "As a code composer, I dance with algorithms, orchestrating symphonies of efficiency and functionality. With each keystroke, I strive to create clean, scalable, and maintainable software architectures, harnessing the power of cutting-edge technologies and frameworks.",
+    profile: "Hello, I'm Michael Murwayi, a passionate and dedicated software developer with a strong foundation in creating efficient, scalable, and user-friendly applications. With 6 years of experience in the field, I have a deep understanding of software development principles and a proven track record of delivering high-quality code. My expertise lies in Python, allowing me to tackle complex challenges and build robust solutions. I thrive in collaborative environments, working closely with cross-functional teams to analyze requirements, design software architectures, and implement innovative solutions. Throughout my career, I have successfully completed projects ranging from web development to Data Science, showcasing my ability to adapt to different industries and technologies. I'm well-versed in agile methodologies and have a strong commitment to writing clean, maintainable code that meets project objectives and exceeds client expectations.",
+    workExperience: [],
+    schoolExperience: [],
+    skills: [
+        {
+            name:"Html",
+            level: "Proficient"
+        },
+        {
+            name:"CSS",
+            level: "Proficient"
+        },
+        {
+            name:"Python",
+            level: "Intermediate"
+        },
+        {
+            name:"JavaScript",
+            level: "Intermediate"
+        },
+        {
+            name:"Mysql",
+            level: "Intermediate"
+        },
+        {
+            name:"Docker",
+            level: "Intermediate"
+        },
+        {
+            name:"Git",
+            level: "Intermediate"
+        },
+        {
+            name:"MongoDB",
+            level: "Beginner"
+        },
+        {
+            name:"PHP",
+            level: "Beginner"
+        },
+        {
+            name:"Cyber Security",
+            level: "Beginner"
+        }
+    ],
+    references: [],
+    companyName: "huncho Inc"
 };
 
 export default initialState
